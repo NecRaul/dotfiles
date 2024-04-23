27939eee7e59445d99eee9f49e4b5c79b172da48
NecRaul <necraul@kuroneko.dev>
2024-04-23 12:21:05 +0400

    Script for toggling VPN

---
+++ .local/bin/scripts/toggle-vpn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+active_connections=$(nmcli connection show --active | wc -l)
+total_connections=$(nmcli connection show | wc -l)
+
+if [[ $active_connections -eq $total_connections ]]; then
+  nmcli connection down kuroneko-vpn
+else
+  nmcli connection up kuroneko-vpn
+fi
