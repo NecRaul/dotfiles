5782a48658f1a035c3e8f2646bc096d569529b76
NecRaul <necraul@kuroneko.dev>
2026-02-01 01:49:49 +0400

    Fix grep usage in scripts and remove unnecessary escapes

--- .local/bin/scripts/offender-epoch.sh
+++ .local/bin/scripts/offender-epoch.sh
@@ -3,7 +3,7 @@
 tmp_list=$(mktemp)
 fd -t f >"$tmp_list"
 
-rg --pcre2 '(?<=^|/)\d{13,15}(?=[ .m])[^/]*$' "$tmp_list" | sort >offender-epoch-millis
-rg --pcre2 '(?<=^|/)\d{16}(?= )[^/]*$' "$tmp_list" | sort >offender-epoch-micros
+grep -P '(?<=^|/)\d{13,15}(?=[ .m])[^/]*$' "$tmp_list" | sort >offender-epoch-millis
+grep -P '(?<=^|/)\d{16}(?= )[^/]*$' "$tmp_list" | sort >offender-epoch-micros
 
 rm "$tmp_list"
--- .local/bin/scripts/update-install-packages.sh
+++ .local/bin/scripts/update-install-packages.sh
@@ -3,5 +3,5 @@
 pacman -Qneq >~/Documents/Github/Repos/dotfiles/install/pacman.txt
 pacman -Qmeq | grep -v '^paru-debug$' >~/Documents/Github/Repos/dotfiles/install/aur.txt
 uv tool list --show-extras |
-  command grep -E '^[a-z]' |
-  command sed -E 's/ v[^ ]+//; s/ \[extras: ([^]]+)\]/[\1]/; s/, +/,/g' >~/Documents/Github/Repos/dotfiles/install/uv.txt
+  grep -E '^[a-z]' |
+  sed -E 's/ v[^ ]+//; s/ \[extras: ([^]]+)\]/[\1]/; s/, +/,/g' >~/Documents/Github/Repos/dotfiles/install/uv.txt
