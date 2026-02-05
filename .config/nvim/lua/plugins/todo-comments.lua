7ebb2425127e5babb600b903b81851e4c24d215e
NecRaul <necraul@kuroneko.dev>
2026-02-05 04:59:43 +0400

    Update config for Nvim Todo Comments

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,9 +1,11 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
     config = function()
         local todo_comments = require("todo-comments")
         todo_comments.setup()
+        vim.keymap.set("n", "<leader>tdf", "<cmd>TodoFzfLua<cr>", { desc = "[T]o[D]o [F]zfLua" })
+        vim.keymap.set("n", "<leader>tdt", "<cmd>TodoTrouble<cr>", { desc = "[T]o[D]o [T]rouble" })
     end,
 }
