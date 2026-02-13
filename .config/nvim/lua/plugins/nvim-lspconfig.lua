3adf3a923dd9a74c468df3183555bbda6532f5ce
NecRaul <necraul@kuroneko.dev>
2026-02-13 16:21:10 +0400

    Disable nvim-lspconfig highlighting

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -69,33 +69,33 @@ return {
                     desc = "Toggle [F]loating Diagnostic",
                 })
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if client and client:supports_method("textDocument/documentHighlight", event.buf) then
-                    local highlight_augroup = vim.api.nvim_create_augroup("nvim-lsp-highlight", {
-                        clear = false,
-                    })
-                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.document_highlight,
-                    })
-                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                        buffer = event.buf,
-                        group = highlight_augroup,
-                        callback = vim.lsp.buf.clear_references,
-                    })
-                    vim.api.nvim_create_autocmd("LspDetach", {
-                        group = vim.api.nvim_create_augroup("nvim-lsp-detach", {
-                            clear = true,
-                        }),
-                        callback = function(event2)
-                            vim.lsp.buf.clear_references()
-                            vim.api.nvim_clear_autocmds({
-                                group = "nvim-lsp-highlight",
-                                buffer = event2.buf,
-                            })
-                        end,
-                    })
-                end
+                -- if client and client:supports_method("textDocument/documentHighlight", event.buf) then
+                --     local highlight_augroup = vim.api.nvim_create_augroup("nvim-lsp-highlight", {
+                --         clear = false,
+                --     })
+                --     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                --         buffer = event.buf,
+                --         group = highlight_augroup,
+                --         callback = vim.lsp.buf.document_highlight,
+                --     })
+                --     vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                --         buffer = event.buf,
+                --         group = highlight_augroup,
+                --         callback = vim.lsp.buf.clear_references,
+                --     })
+                --     vim.api.nvim_create_autocmd("LspDetach", {
+                --         group = vim.api.nvim_create_augroup("nvim-lsp-detach", {
+                --             clear = true,
+                --         }),
+                --         callback = function(event2)
+                --             vim.lsp.buf.clear_references()
+                --             vim.api.nvim_clear_autocmds({
+                --                 group = "nvim-lsp-highlight",
+                --                 buffer = event2.buf,
+                --             })
+                --         end,
+                --     })
+                -- end
                 if client and client:supports_method("textDocument/inlayHint", event.buf) then
                     vim.keymap.set("n", "<leader>th", function()
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({
