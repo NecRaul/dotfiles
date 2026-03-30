9050511b62fffd80c5f2ba2e164222dab025c6e9
NecRaul <necraul@kuroneko.dev>
2026-03-31 03:26:28 +0400

    Replace gallery-dl[extra] with git URL in uv tool list

--- .local/bin/scripts/update-install-packages.sh
+++ .local/bin/scripts/update-install-packages.sh
@@ -4,4 +4,5 @@ pacman -Qneq >~/Documents/Github/Repos/dotfiles/install/pacman.txt
 pacman -Qmeq | grep -v '^paru-debug$' >~/Documents/Github/Repos/dotfiles/install/aur.txt
 uv tool list --show-extras |
   grep -E '^[a-z]' |
-  sed -E 's/ v[^ ]+//; s/ \[extras: ([^]]+)\]/[\1]/; s/, +/,/g' >~/Documents/Github/Repos/dotfiles/install/uv.txt
+  sed -E 's/ v[^ ]+//; s/ \[extras: ([^]]+)\]/[\1]/; s/, +/,/g; s#^gallery-dl\[extra\]#git+https://github.com/NecRaul/gallery-dl[extra]#' \
+    >~/Documents/Github/Repos/dotfiles/install/uv.txt
