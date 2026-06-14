ac715b5f5a2198fd94e8a7634161ce5653e7e4ea
NecRaul <necraul@kuroneko.dev>
2026-06-14 07:25:06 +0400

    Apply consistent shfmt/editorconfig formatting across scripts

--- .local/bin/scripts/update-install-packages.sh
+++ .local/bin/scripts/update-install-packages.sh
@@ -3,6 +3,6 @@
 pacman -Qneq >~/Documents/Github/Repos/dotfiles/install/pacman.txt
 pacman -Qmeq | grep -v '^paru-debug$' >~/Documents/Github/Repos/dotfiles/install/aur.txt
 uv tool list --show-extras |
-  grep -E '^[a-z]' |
-  sed -E 's/ v[^ ]+//; s/ \[extras: ([^]]+)\]/[\1]/; s/, +/,/g; s#^gallery-dl\[extra\]#git+https://github.com/NecRaul/gallery-dl[extra]#' \
-    >~/Documents/Github/Repos/dotfiles/install/uv.txt
+    grep -E '^[a-z]' |
+    sed -E 's/ v[^ ]+//; s/ \[extras: ([^]]+)\]/[\1]/; s/, +/,/g; s#^gallery-dl\[extra\]#git+https://github.com/NecRaul/gallery-dl[extra]#' \
+        >~/Documents/Github/Repos/dotfiles/install/uv.txt
