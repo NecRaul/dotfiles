ee4169e41c763f861b13eba3cba1afb983d996ff
NecRaul <necraul@kuroneko.dev>
2026-07-17 04:17:30 +0400

    style(scripts): reformat uv tool upgrade pipeline

--- home/.local/bin/scripts/uv-upgrade.sh
+++ home/.local/bin/scripts/uv-upgrade.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-uv tool list | grep -E '^[a-z]' | cut -d' ' -f1 | while read -r tool; do
-    echo "Upgrading $tool"
-    uv tool upgrade "$tool"
-    echo
-done
+uv tool list |
+    grep -E '^[a-z]' |
+    cut -d' ' -f1 |
+    while read -r tool; do
+        echo "Upgrading $tool"
+        uv tool upgrade "$tool"
+        echo
+    done
