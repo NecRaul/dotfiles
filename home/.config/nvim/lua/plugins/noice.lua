83143a4e65df3c082dfa0bfd11fc61c4c16c1af5
NecRaul <necraul@kuroneko.dev>
2026-08-02 04:18:22 +0400

    feat(nvim): add noice plugin

---
+++ home/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,11 @@
+return {
+    "folke/noice.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    config = function()
+        local noice = require("noice")
+        noice.setup()
+    end,
+}
